1d697aa5c8a41a7a1a5a46f8d7d7d475200e9dac
MichailKr <miwaggg8@gmail.com>
2025-06-06 14:15:50 +0700

    основа игры

--- Happy_farm/main.py
+++
@@ -1,8 +0,0 @@
-from src.game_manager import GameManager
-
-def main():
-    game = GameManager()
-    game.run()
-
-if __name__ == "__main__":
-    main()
--- Happy_farm/src/game_state.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class GameState(Enum):
-    MENU = 0
-    SETTINGS = 1
-    GAME = 2
-    PAUSE = 3  # Добавим еще и состояние паузы
